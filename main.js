a4fd55645c35cd1dd1dd9b8e84607a977197c4fc
Adam Woolridge <a.woolridge@gmail.com>
2016-07-06 17:44:57 +0100

    repairers

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var rolerRepairer = require('role.repairer');
 
 module.exports.loop = function ()
 {
@@ -21,15 +22,29 @@ module.exports.loop = function ()
             roleUpgrader.run(creep);
         else if (creep.memory.role == 'builder')
             roleBuilder.run(creep);
+        else if (creep.memory.role == 'repairer')
+            rolerRepairer.run(creep);
     }
 
-    var minHarvesters = 10;
+    var minHarvesters = 6;
     var numOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
 
-    var minUpgraders = 10;
+    var minUpgraders = 3;
     var numOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
 
+    var minRepairers = 2;
+    var numOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
+
+    var minBuilders = 5;
+    var numOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+
     var name = undefined;
+
+    console.log('Harvesters: ' + numOfHarvesters);
+    console.log('Upgraders: ' + numOfUpgraders);
+    console.log('Repairers: ' + numOfRepairers);
+    console.log('Builders: ' + numOfBuilders);
+    console.log('-------');
 
     if (numOfHarvesters < minHarvesters)
     {
@@ -39,10 +54,15 @@ module.exports.loop = function ()
     {
         name = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE, MOVE], undefined, { role: 'upgrader', working: false} );
     }
-    else
+    else if (numOfRepairers < minRepairers)
     {
-        name = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE, MOVE], undefined, { role: 'builder', working: false} );
+        name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, { role: 'repairer', working: false} );
     }
+    else if (numOfBuilders < minBuilders)
+    {
+        name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, { role: 'builder', working: false} );
+    }
+
 
     if (_.isString(name))
         console.log('Spawned new ' + Memory.creeps[name].role + ' called ' + name);
