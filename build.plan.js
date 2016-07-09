d4acf2ccdf3ee17b998ca5d04edb176cda59d1c4
Adam Woolridge <a.woolridge@gmail.com>
2016-07-09 01:13:43 +0100

    now autoplaces roads to sources

--- build.plan.js
+++ build.plan.js
@@ -13,6 +13,8 @@ var buildPlan = [{
 module.exports = {
     run: function(spawner)
     {
+        pickExtensionLocations();
+
         var nextToBuild = undefined;
 
         // Build a list of all the structures we already have
@@ -25,7 +27,6 @@ module.exports = {
 
         console.log("Existing structures:")
         console.log(existingStructures);
-
 
         // Now put all the planned structures upto the current CL(including ones that may have been built) into an array
         var planStructures = [];
@@ -62,3 +63,24 @@ module.exports = {
     }
 };
 
+function pickExtensionLocations()
+{
+    var sources = Game.spawns.Spawn1.room.find(FIND_SOURCES);
+
+    for (let i=0; i<sources.length; i++)
+    {
+        var path = Game.spawns.Spawn1.pos.findPathTo(sources[i].pos,  {ignoreCreeps: true});
+
+        for (let p=0; p<path.length; p++)
+        {
+            Game.spawns.Spawn1.room.createConstructionSite(path[p].x, path[p].y, STRUCTURE_ROAD);
+        }
+        //console.log(path[path.length-2].x, path[path.length-2].y);
+    }
+    //console.log(sources);
+
+    // var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => (s.structureType==STRUCTURE_EXTENSION || s.structureType==STRUCTURE_SPAWN) && s.energy < s.energyCapacity});
+    //
+    // if (structure != undefined)
+    // {
+}
